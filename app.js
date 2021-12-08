fadb83e581feb53386b1a2099c1814097be338b2
arief sangga <sangga@ariefs-MacBook-Pro.local>
2021-12-08 09:38:41 +0700

    deploy heroku

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const session = require('express-session');
 const flash = require('connect-flash');
 // import mongoose
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/server_hotel', {
+mongoose.connect('mongodb+srv://kapoylawas:gr11mj00w@cluster0.l8r4d.mongodb.net/db_staycation?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
